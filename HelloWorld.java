abbd02c854ecaef04d4ddaf2abac6e7d261acddd
Yura <yyyura@gmail.com>
2016-10-18 12:10:19 +0100

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {//same name as Hello.java
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("hello <name>");
+	  
+   }
+}
